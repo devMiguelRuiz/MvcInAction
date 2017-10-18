33a52b395ee94be12cdf0d1bce680dc3d691a363
Miguel Ruiz <24594999+miguelEpam@users.noreply.github.com>
2017-10-18 00:00:35 -0500

    Adding Custom demo Exception filters

---
+++ Utilities/Filters/CustomExceptionFilter.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace MvcInAction.Utilities.Filters
+{
+    public class CustomExceptionFilter : FilterAttribute, IExceptionFilter
+    {
+        public void OnException(ExceptionContext filterContext)
+        {
+            filterContext.ExceptionHandled = true;
+
+            var dictionary = new ViewDataDictionary
+            {
+                {"Controller", filterContext.RouteData.Values["controller"]},
+                {"Action", filterContext.RouteData.Values["action"]},
+                {"Error", filterContext.Exception.Message}
+            };
+
+            filterContext.Result = new ViewResult
+            {
+                ViewName = "Error",
+                ViewData = new ViewDataDictionary(dictionary)
+            };
+        }
+    }
+}
---
+++ Utilities/Filters/CustomOverridedExceptionFilter.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+
+namespace MvcInAction.Utilities.Filters
+{
+    public class CustomOverridedExceptionFilter : HandleErrorAttribute
+    {
+        public override void OnException(ExceptionContext filterContext)
+        {
+            filterContext.ExceptionHandled = true;
+
+            var dictionary = new ViewDataDictionary
+            {
+                {"Controller", filterContext.RouteData.Values["controller"]},
+                {"Action", filterContext.RouteData.Values["action"]},
+                {"Error", filterContext.Exception.Message}
+            };
+
+            filterContext.Result = new ViewResult
+            {
+                ViewName = "Error",
+                ViewData = new ViewDataDictionary(dictionary)
+            };
+
+            base.OnException(filterContext);
+        }
+    }
+}
