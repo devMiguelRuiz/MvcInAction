6d4ce46d720e784cafc4b12fb49e05b6ca100a0d
Miguel Ruiz <24594999+miguelEpam@users.noreply.github.com>
2017-10-23 23:25:35 -0500

    Updating ConnectionString to use LocalDB

--- MvcInAction.Data/RepositoryContext.cs
+++ MvcInAction.Data/RepositoryContext.cs
@@ -5,6 +5,10 @@ namespace MvcInAction.Data
 {
     public class RepositoryContext : DbContext
     {
+        public RepositoryContext(): base("MvcInActionDb")
+        {
+            
+        }
         public DbSet<Contact> Contacts { get; set; }
         public DbSet<Company> Companies { get; set; }
     }
